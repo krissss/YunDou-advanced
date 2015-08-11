829b772138f89872bf99e91b9255f1fce7f424f6
462679766@qq.com <asd7798>
2015-08-11 16:56:28 +0800

    微信端  注册页面边距调整

--- frontend/views/account/register.php
+++ frontend/views/account/register.php
@@ -12,32 +12,34 @@ $openId = $session->get('openId');
 ?>
 
 <div class="alert alert-info" role="alert">为了更好的为您提供服务，请认真进行实名认证！</div>
-<?php $form = ActiveForm::begin([
-    'id' => 'account-register',
-    'options' => ['class' => 'form-horizontal'],
-    'fieldConfig' => [
-        'options' => ['class' => 'form-group no-margin-bottom'],
-        'template' => "{label}<div class='col-xs-9 no-padding-left'>{input}</div><div class='col-xs-9 col-xs-offset-3'>{error}</div>",
+<div class="container-fluid">
+    <?php $form = ActiveForm::begin([
+        'id' => 'account-register',
+        'options' => ['class' => 'form-horizontal'],
+        'fieldConfig' => [
+            'options' => ['class' => 'form-group no-margin-bottom'],
+            'template' => "{label}<div class='col-xs-9 no-padding-left'>{input}</div><div class='col-xs-9 col-xs-offset-3'>{error}</div>",
+            'labelOptions' => ['class'=>'col-xs-3 control-label'],
+            //'inputOptions' => ['class' => 'col-sm-10'],
+            //'inline' => true,
+        ],
+    ]) ?>
+    <?= $form->field($registerForm, 'nickname')->textInput(['value'=>$user['nickname']]) ?>
+    <?= $form->field($registerForm, 'realname') ?>
+    <?= $form->field($registerForm, 'provinceId')->dropDownList(ArrayHelper::map($provinces,'provinceId','name'),['prompt'=>'请选择'])?>
+    <?= $form->field($registerForm, 'majorJobId')->dropDownList(ArrayHelper::map($majorJobs,'majorJobId','name'),['prompt'=>'请选择'])?>
+    <?= $form->field($registerForm, 'cellphone') ?>
+    <?= $form->field($registerForm, 'yzm', [
+        'template' => "{label}<div class='col-xs-5 no-padding-left'>{input}</div><div class='col-xs-4 no-padding-left'><span class='btn btn-primary'>获取验证码</span></div><div class='col-xs-9 col-xs-offset-3'>{error}</div>",
         'labelOptions' => ['class'=>'col-xs-3 control-label'],
-        //'inputOptions' => ['class' => 'col-sm-10'],
-        //'inline' => true,
-    ],
-]) ?>
-<?= $form->field($registerForm, 'nickname')->textInput(['value'=>$user['nickname']]) ?>
-<?= $form->field($registerForm, 'realname') ?>
-<?= $form->field($registerForm, 'provinceId')->dropDownList(ArrayHelper::map($provinces,'provinceId','name'),['prompt'=>'请选择'])?>
-<?= $form->field($registerForm, 'majorJobId')->dropDownList(ArrayHelper::map($majorJobs,'majorJobId','name'),['prompt'=>'请选择'])?>
-<?= $form->field($registerForm, 'cellphone') ?>
-<?= $form->field($registerForm, 'yzm', [
-    'template' => "{label}<div class='col-xs-5 no-padding-left'>{input}</div><div class='col-xs-4 no-padding-left'><span class='btn btn-primary'>获取验证码</span></div><div class='col-xs-9 col-xs-offset-3'>{error}</div>",
-    'labelOptions' => ['class'=>'col-xs-3 control-label'],
-]) ?>
-<?= $form->field($registerForm, 'company') ?>
-<?= $form->field($registerForm, 'address') ?>
-<?= $form->field($registerForm, 'tjm') ?>
-<div class="form-group">
-    <div class="col-xs-offset-3 col-xs-9 no-padding-left">
-        <button type="submit" class="btn btn-primary">提交</button>
+    ]) ?>
+    <?= $form->field($registerForm, 'company') ?>
+    <?= $form->field($registerForm, 'address') ?>
+    <?= $form->field($registerForm, 'tjm') ?>
+    <div class="form-group">
+        <div class="col-xs-offset-3 col-xs-9 no-padding-left">
+            <button type="submit" class="btn btn-primary">提交</button>
+        </div>
     </div>
+    <?php ActiveForm::end() ?>
 </div>
-<?php ActiveForm::end() ?>
