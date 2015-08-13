e948b9a265c2e2a84f3f11c9115f9ced9d0df756
462679766@qq.com <asd7798>
2015-08-13 13:07:23 +0800

    微信端  推荐页面  jssdk测试

--- frontend/views/account/recommend.php
+++ frontend/views/account/recommend.php
@@ -23,6 +23,15 @@ $currentUrl = explode('#',urldecode(Url::current([],true)))[0];
             'onMenuShareTimeline',    //分享到朋友圈
         ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
     });
+    wx.checkJsApi({
+        jsApiList: ['onMenuShareTimeline'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+        success: function(res) {
+            console.log(res);
+            alert(res);
+            // 以键值对的形式返回，可用的api值true，不可用为false
+            // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+        }
+    });
     wx.error(function(res){
         console.log(res);
     });
