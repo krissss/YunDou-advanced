7822c8682a064c7d9254d043143ca5f1e4f76861
462679766@qq.com <asd7798>
2015-08-17 10:04:57 +0800

    微信端  授权跳转页面不正常修复

--- frontend/filters/OpenIdFilter.php
+++ frontend/filters/OpenIdFilter.php
@@ -25,6 +25,7 @@ class OpenIdFilter extends ActionFilter
         }
         $request = Yii::$app->request;
         $state = $request->get("state");
+        $current_url = Url::current([],true);   //认证后跳转至当前网址
         if($state == 'YUN'){    //需要与getAuthorizeUrl($redirect_uri)中定义的一致，用户认证后回调操作如下
             $code = $request -> get("code");
             if($code){  //code存在表示用户允许授权
@@ -32,13 +33,14 @@ class OpenIdFilter extends ActionFilter
                 $openId = $result->openid;
                 $access_token = $result->access_token;
                 $session->set('openId',$openId);
-                return parent::beforeAction($action);
+                header("Location:$current_url");    //授权后需重新跳转到当前网页
+                return false;
             }else{
                 echo "用户不允许授权";
                 return false;
             }
         }
-        $redirect_uri = urlencode(Url::current([],true));   //认证后跳转至当前网址
+        $redirect_uri = urlencode($current_url);   //认证后跳转至当前网址
         $url = WeiXinFunctions::getAuthorizeUrl($redirect_uri);
         header("Location:$url");
         return false;
