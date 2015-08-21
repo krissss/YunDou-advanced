336f8226fd99492f7e06d36037743950936126e5
462679766@qq.com <asd7798>
2015-08-21 11:03:29 +0800

    微信端   支付接口测试

--- frontend/views/account/recharge-order.php
+++ frontend/views/account/recharge-order.php
@@ -4,6 +4,7 @@
 $tools = new JsApiPay();
 /** @var $jsApiParameters String //JSON */
 $jsApiParameters = $tools->GetJsApiParameters($order);
+$jsApiParameters = json_decode($jsApiParameters);
 
 //获取共享收货地址js函数参数
 /** @var $editAddress String //JSON */
@@ -18,13 +19,46 @@ $editAddress = $tools->GetEditAddressParameters();
  */
 
 ?>
-<script type="text/javascript">
+<?php
+use yii\helpers\Url;
+use frontend\functions\WeiXinFunctions;
+$timestamp = time();
+$currentUrl = explode('#',urldecode(Url::current([],true)))[0];
+?>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+<script>
+    wx.config({
+        debug: true,
+        appId: '<?=WeiXinFunctions::getAppId()?>',
+        timestamp: <?=$timestamp?>,
+        nonceStr: 'yundou-js',
+        signature: '<?=WeiXinFunctions::generateJsSignature($currentUrl,$timestamp)?>',
+        jsApiList: [
+            'onMenuShareTimeline',    //分享到朋友圈
+            'chooseWXPay',    //分享到朋友圈
+        ]
+    });
+    wx.ready(function(){
+        wx.chooseWXPay({
+            timestamp: 0, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+            nonceStr: '<?=$jsApiParameters->nonceStr?>', // 支付签名随机串，不长于 32 位
+            package: '<?=$jsApiParameters->package?>', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+            signType: '<?=$jsApiParameters->signType?>', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+            paySign: '<?=$jsApiParameters->paySign?>', // 支付签名
+            success: function (res) {
+                // 支付成功后的回调函数
+                alert(res.err_msg);
+            }
+        });
+    });
+</script>
+<!--<script type="text/javascript">
     //调用微信JS api 支付
     function jsApiCall()
     {
         WeixinJSBridge.invoke(
             'getBrandWCPayRequest',
-            <?php echo $jsApiParameters; ?>,
+            <?php /*echo $jsApiParameters; */?>,
             function(res){
                 if(res.err_msg == "get_brand_wcpay_request：ok" ) {
 
@@ -47,7 +81,7 @@ $editAddress = $tools->GetEditAddressParameters();
         }
     }
 
-</script>
+</script>-->
 <?php
 echo '<font color="#f00"><b>统一下单支付单信息</b></font><br/>';
 //打印输出数组信息
