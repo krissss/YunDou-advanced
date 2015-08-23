74f383e34fbce8b8b47666460b52262bc872440b
462679766@qq.com <asd7798>
2015-08-23 12:35:30 +0800

    微信端   修复我的账户下云豆余额不变的bug

--- frontend/views/account/index.php
+++ frontend/views/account/index.php
@@ -4,6 +4,7 @@
 use yii\helpers\Url;
 use common\models\IncomeConsume;
 use common\functions\CommonFunctions;
+use common\models\Users;
 
 $this->title = "我的账户";
 
@@ -15,7 +16,7 @@ $userIcon = CommonFunctions::createHttpImagePath($userIcon);
     <div class="avatar">
         <img src="<?=$userIcon?>" alt="head">
         <p><?=$user['nickname']?></p>
-        <p>云豆余额：<strong><?=$user['bitcoin']?></strong></p>
+        <p>云豆余额：<strong><?=Users::findBitcoin($user['userId'])?></strong></p>
     </div>
 </div>
 <hr>
