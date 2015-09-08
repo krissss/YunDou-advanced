b9185c71b85c625a85f78b652790aa885b4f5b77
kriss <462679766@qq.com>
2015-09-08 10:28:00 +0800

    后台  登录页面完成

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -12,29 +12,33 @@ $this->params['breadcrumbs'] = [
 ];
 ?>
 <div class="login-container animated fadeInDown">
-    <div class="loginbox bg-white">
-        <div class="loginbox-title">
-            <h4>登录</h4>
+    <div class="login-content">
+        <div class="loginImg pull-left"></div>
+        <div class="loginbox pull-right bg-white">
+            <div class="loginbox-title">
+                <h4>登录</h4>
+            </div>
+            <?php $form = ActiveForm::begin([
+                'fieldConfig' => [
+                    'template' => "{input}{error}",
+                ],
+            ]); ?>
+            <div class="loginbox-textbox">
+                <?= $form->field($model, 'username')->textInput(['placeholder'=>'用户名']) ?>
+            </div>
+            <div class="loginbox-textbox">
+                <?= $form->field($model, 'password')->passwordInput(['placeholder'=>'密码']) ?>
+            </div>
+            <div class="loginbox-textbox">
+                <?= $form->field($model, 'verifyCode')->widget(\yii\captcha\Captcha::className(), [
+                    'template' => '<div class="row"><div class="col-sm-6">{image}</div><div class="col-sm-6">{input}</div></div>',
+                ]);?>
+            </div>
+            <div class="loginbox-submit">
+                <input type="submit" class="btn btn-primary btn-block" value="登录">
+            </div>
+            <?php ActiveForm::end(); ?>
         </div>
-        <?php $form = ActiveForm::begin([
-            'fieldConfig' => [
-                'template' => "{input}{error}",
-            ],
-        ]); ?>
-        <div class="loginbox-textbox">
-            <?= $form->field($model, 'username')->textInput(['placeholder'=>'用户名']) ?>
-        </div>
-        <div class="loginbox-textbox">
-            <?= $form->field($model, 'password')->passwordInput(['placeholder'=>'密码']) ?>
-        </div>
-        <div class="loginbox-textbox">
-            <?= $form->field($model, 'verifyCode')->widget(\yii\captcha\Captcha::className(), [
-                'template' => '<div class="row"><div class="col-sm-6">{image}</div><div class="col-sm-6">{input}</div></div>',
-            ]);?>
-        </div>
-        <div class="loginbox-submit">
-            <input type="submit" class="btn btn-primary btn-block" value="登录">
-        </div>
-        <?php ActiveForm::end(); ?>
+        <div class="clearfix"></div>
     </div>
 </div>
