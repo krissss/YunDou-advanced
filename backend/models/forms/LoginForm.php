d2c067fb069dd23e7c97c64138b8fd8d6d482cb5
kriss <462679766@qq.com>
2015-09-08 10:54:08 +0800

    用户登录  限制被终止的用户登录

--- backend/models/forms/LoginForm.php
+++ backend/models/forms/LoginForm.php
@@ -12,6 +12,8 @@ class LoginForm extends Model{
     public $password;
     public $verifyCode;
 
+    private $_user = false;
+
     public function rules(){
         return [
             [['username','password'],'required'],
@@ -31,13 +33,21 @@ class LoginForm extends Model{
     }
 
     public function login(){
-        $user = Users::find()->where(['username'=>$this->username,'password'=>CommonFunctions::encrypt($this->password)])->one();
+        $user = Users::find()->where(['username'=>$this->username])->one();
         if($user){
+            if($user['state'] == Users::STATE_STOP){    //用户状态被终止
+                $this->addError('username',"该用户已被终止登录");
+                return false;
+            }
+            if($user['password'] != CommonFunctions::encrypt($this->password)){
+                $this->addError('password',"密码错误");
+                return false;
+            }
             Yii::$app->session->set('user',$user);
             return true;
+        }else{
+            $this->addError('username',"用户不存在");
+            return false;
         }
-        $this->addError('username',"用户名或密码错误");
-        $this->addError('password',"用户名或密码错误");
-        return false;
     }
 }
