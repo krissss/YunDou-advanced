e01887f72fb918d75b892f9575d49c1d43c08a5c
kriss <462679766@qq.com>
2015-09-10 16:07:52 +0800

    微信端  支付通知测试

--- frontend/web/notify.php
+++ frontend/web/notify.php
@@ -1,6 +1,10 @@
 <?php
-$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+include_once("../../common/functions/CommonFunctions.php");
+$postStr = isset($GLOBALS["HTTP_RAW_POST_DATA"])?$GLOBALS["HTTP_RAW_POST_DATA"]:"";
 if(!empty($postStr)){
+    include_once("../functions/WxPayFunctions.php");
     \common\functions\CommonFunctions::logger_wx($postStr);
     \frontend\functions\WxPayFunctions::payNotify($postStr);
+}else{
+    \common\functions\CommonFunctions::logger_wx("收到空的通知");
 }
