cd3e8e40fc92c523c5c96dc1ae0d119365e4f10c
kriss <462679766@qq.com>
2015-09-11 08:24:48 +0800

    微信端  支付通知测试

--- frontend/functions/WxPayFunctions.php
+++ frontend/functions/WxPayFunctions.php
@@ -17,10 +17,15 @@ class WxPayFunctions
     public static function payNotify($xml){
         $cache = \Yii::$app->cache;
         $xmlObj = json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+        CommonFunctions::logger_wx($xmlObj);
+        foreach($xmlObj as $a){
+            CommonFunctions::logger_wx($a);
+        }
         if($xmlObj->return_code == 'SUCCESS'){
             $transaction_id = $xmlObj->transaction_id;
             if('ok' == $cache->get($transaction_id)){
                 CommonFunctions::logger_wx("订单:".$transaction_id."，已处理完，重复通知");
+                return 'success';
             } else {
                 CommonFunctions::logger_wx("订单:".$transaction_id."，首次记录");
                 $cache->set($transaction_id,'ok',24*3600);  //缓存1天
@@ -32,6 +37,7 @@ class WxPayFunctions
                 $addBitcoin = intval($money) * $proportion; //计算应得的云豆数
                 Money::recordOne($user, $money, $addBitcoin, Money::TYPE_PAY, Money::FROM_WX);   //记录充值记录+返点+收入支出表变化+用户云豆数增加
                 CommonFunctions::logger_wx("订单:".$transaction_id."，".$openId."pay￥".$money);
+                return 'success';
             }
         }else{
             CommonFunctions::logger_wx("错误消息:".$xmlObj->return_msg);
