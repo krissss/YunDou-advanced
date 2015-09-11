728c9336028a1bd4c91a7c98df807d1943505e34
kriss <462679766@qq.com>
2015-09-11 10:57:42 +0800

    微信端  支付通知完成

--- frontend/controllers/WeChatController.php
+++ frontend/controllers/WeChatController.php
@@ -14,15 +14,4 @@ class WeChatController extends Controller
         //$wechat->valid();
         $wechat->responseMsg();
     }
-
-    /** 微信支付后接受通知的地址  */
-    public function actionNotify(){
-        $postStr = isset($GLOBALS["HTTP_RAW_POST_DATA"])?$GLOBALS["HTTP_RAW_POST_DATA"]:"";
-        if(!empty($postStr)) {
-            \common\functions\CommonFunctions::logger_wx($postStr);
-            \frontend\functions\WxPayFunctions::payNotify($postStr);
-        }else{
-            CommonFunctions::logger_wx("接收到通知");
-        }
-    }
 }
